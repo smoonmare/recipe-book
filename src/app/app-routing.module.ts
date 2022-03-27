51c466659d450748201c02bfc2fd406e912b7178
smoonmare <smoonmare@gmail.com>
2022-03-26 20:16:28 -0700

    Added main Router paths. Refactored code to use routerOutlet.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipeBookComponent},
+  {path: 'shopping-list', component: ShoppingListComponent},
+  {path: '**', redirectTo: '/recipes', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
