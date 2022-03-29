d2b724e2f8e5652611cbe3ce69f9ffea6e95c240
smoonmare <smoonmare@gmail.com>
2022-03-28 21:03:57 -0700

    Genereted RecipeEditCompoenent. Created children route for Recipes route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
+
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipeBookComponent, children: [
     {path: '', component: RecipeStartComponent},
-    {path: ':id', component: RecipeDetailComponent}
+    {path: 'new', component: RecipeEditComponent},
+    {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent}
   ]},
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: '**', redirectTo: '/recipes', pathMatch: 'full'}
