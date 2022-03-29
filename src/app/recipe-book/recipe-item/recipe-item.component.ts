b32fb8b5f895d5fc9475854674097cde37864314
smoonmare <smoonmare@gmail.com>
2022-03-28 20:51:42 -0700

    Activated RecipeItem link. Passing index(id) as a property of RecipeListComponent to the RecipeItemComponent.

--- src/app/recipe-book/recipe-item/recipe-item.component.ts
+++ src/app/recipe-book/recipe-item/recipe-item.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.interface';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe?: Recipe;
+  @Input() index?: number;
 
   constructor() { }
 
