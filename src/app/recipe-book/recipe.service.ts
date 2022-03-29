e34cdbced2875768d324dbe33c5176274b24d029
smoonmare <smoonmare@gmail.com>
2022-03-28 23:44:38 -0700

    Removed unused EventEmitter in RecipeService. Now the navigation is handled by Router.

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.interface';
 import { Ingredient } from '../shared/ingredient.interface';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -68,7 +68,6 @@ export class RecipeService {
       ]
     }
   ];
-  recipeSelected = new EventEmitter<Recipe>();
 
   constructor(private shoppingService: ShoppingListService) { }
 
