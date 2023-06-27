3c11c201213faf6929447f4943401073f3344566
smoonmare <smoonmare@gmail.com>
2023-06-27 16:55:21 -0700

    Title: Enhance Authentication Component with Toggle between Login and Signup
    Description:
    This commit enhances the authentication component by implementing a toggle functionality between login and signup modes. The user can now switch between the login and signup forms within the same component, by clicking the 'Switch to SignUp/Login' button. The button labels dynamically change based on the selected mode.
    
    Changes:
    
    - Updated the 'auth.component.html' template to bind button labels dynamically depending on the current mode (login or signup).
    - Introduced a new method 'onSwitchMode' in 'auth.component.ts' to toggle the boolean flag 'isLoginModeOn'.
    - Added a boolean property 'isLoginModeOn' to 'auth.component.ts' to keep track of the current mode.
    
    This enhancement improves the user experience by allowing them to switch between login and signup within the same component seamlessly.

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginModeOn = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSwithMode() {
+    this.isLoginModeOn = !this.isLoginModeOn;
   }
 
 }
