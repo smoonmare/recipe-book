f8deb9ac8d06cff9e8b06819893c8afc4feb505a
smoonmare <smoonmare@gmail.com>
2023-06-29 13:09:48 -0700

    feat(auth): Add login functionality and refactor auth component
    - Implement login method in AuthService which sends a request to Firebase for authenticating the user.
    - Refactor AuthComponent to use a single Observable for both login and sign-up, reducing code duplication.
    - Extract error handling in AuthService to a reusable private method called handleError.
    - Update AuthComponent to reset form after submission.
    
    This commit introduces the user login functionality and enhances the code structure in the authentication feature by optimizing error handling and streamlining the observable subscriptions.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
-import { Subject } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -27,25 +27,25 @@ export class AuthComponent implements OnInit {
 
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>;
 
     this.isLoading = true;
     if (this.isLoginModeOn) {
-      // ...
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService
-      .signUp(email, password)
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-          this.isLoading = false;
-        },
-        error: (errorMessage) => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      });
+      authObs = this.authService.signUp(email, password);
     }
+    authObs.subscribe({
+      next: (res) => {
+        console.log(res);
+        this.isLoading = false;
+      },
+      error: (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    });
     form.reset();
   }
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { key as apiKey} from 'src/environments/environment';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { key as apiKey } from 'src/environments/environment';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean
 }
 
 @Injectable({
@@ -20,25 +21,48 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   signUp(email: string, password: string) {
-     return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-      }
-    ).pipe(
-      catchError(errorRes => {
-        let errorMessage = 'An ukknown error occured. Please try again later.';
-        if (!errorRes.error || !errorRes.error.error) {
-          return throwError(() => errorMessage);
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
         }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email alrede exists on our service. Plase try using a different one.';
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
         }
-        return throwError(() => errorMessage);
-      })
-    );
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An ukknown error occured. Please try again later.';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => errorMessage);
+    }
+    console.log('Error:', errorRes.error.error.message);
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email alrede exists on our service. Plase try using a different one.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Invalid crenedtials. Please try with a different credentials or simply Sign Up.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Invalid crenedtials. Please try with a different credentials or simply Sign Up.';
+        break;
+    }
+    return throwError(() => errorMessage);
   }
 }
