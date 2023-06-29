a710ef6d6ae9170877df738d0b11ba3155bf988f
smoonmare <smoonmare@gmail.com>
2023-06-29 13:49:46 -0700

    feat(auth): Implement navigation after authentication and update header
    - Use Angular Router in AuthComponent to navigate to the recipes page after successful authentication.
    - Conditionally render header links based on the authentication state.
    - Listen to the authentication state in the HeaderComponent and update the 'isAuthenticated' property.
    - Implement 'OnDestroy' lifecycle hook to clean up subscriptions.
    
    This commit enhances user experience by updating navigation and header based on the authentication state.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
 import { Observable, Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,10 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
@@ -39,6 +43,7 @@ export class AuthComponent implements OnInit {
       next: (res) => {
         console.log(res);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       },
       error: (errorMessage) => {
         console.log(errorMessage);
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private subscriptions: Subscription[] = [];
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.subscriptions.push(
+      this.authService.user.subscribe(user => {
+        this.isAuthenticated = !!user;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   onSaveData() {
